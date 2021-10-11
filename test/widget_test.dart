209d437d6bc743d545b915c03e3b1a5fb1cc8ed8
Richard <richardwang1134@gmail.com>
2021-10-12 00:08:52 +0800

    Update main.dart to flutter webview example

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:healthcheck_autofill/main.dart';
+import 'package:health_check_autofill/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(WebViewExample());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
